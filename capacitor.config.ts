a3349e503de5b3f6b73f6004cf88f7f7e16e3cd2
Manu-Andaluz <manupeandaluz@gmail.com>
2024-04-06 01:58:15 -0300

    adding icons for android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-app-base',
+  appId: 'guardkey.test.starter',
+  appName: 'GuardKey',
   webDir: 'www',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
 };
 
 export default config;
