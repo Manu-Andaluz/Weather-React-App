7a5dc2127ee45c985b61df9ee9493b6d4db74ec6
Manu-Andaluz <manupeandaluz@gmail.com>
2024-04-11 19:39:41 -0300

    fixing problems

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   apiPath: 'http://3.142.130.166:8000',
 };
 
